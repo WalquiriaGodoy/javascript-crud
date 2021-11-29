316e5336f86f77d258c9129ab53c533e45106fb9
Walquiria Godoy <walquiriagodoy@gmail.com>
2021-11-29 17:28:56 -0300

    feat(registration): Delete a customer

--- controller/listaCliente-controller.js
+++ controller/listaCliente-controller.js
@@ -1,6 +1,6 @@
 import { clienteService } from "../service/cliente-service.js"
 
-const criaNovaLinha = (nome, email) => {  // Cria um template
+const criaNovaLinha = (nome, email, id) => {  // Cria um template
     const linhaNovoCliente = document.createElement('tr')
     const conteudo = `<td class="td" data-td>${nome}</td>
                             <td>${email}</td>
@@ -12,13 +12,26 @@ const criaNovaLinha = (nome, email) => {  // Cria um template
                             </td>`
 
     linhaNovoCliente.innerHTML = conteudo
+    linhaNovoCliente.dataset.id = id    //cria um data-attributs
     return linhaNovoCliente
 }
 
 const tabela = document.querySelector('[data-tabela]')  // Percorre o dom e encontra o corpo da tabela
+tabela.addEventListener('click', (evento) => {
+    let eHBotaoDeletar = evento.target.className === 'botao-simples botao-simples--excluir'
+    if (eHBotaoDeletar) {
+        const linhaCliente = evento.target.closest('[data-id]')
+        let id = linhaCliente.dataset.id
+        clienteService.removeCliente(id)
+        .then( ()=> {
+            linhaCliente.remove()
+        })
+    }
+})
+
 
 clienteService.listaClientes()     // Pega os dados da API, faz o looping interando sobre os dados e exibindo na tela
 .then( data => {
     data.forEach(elemento => {
-    tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email))
-    })})  
+    tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email, elemento.id))
+    })})
